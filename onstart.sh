e203a2f0072009133717cffcf4abc457dad66941
Jonathan Shields <jshieldsf@gmail.com>
2025-10-15 18:18:03 -0400

    Remove start JupyterLab script in onscript.sh

--- onstart.sh
+++ onstart.sh
@@ -9,7 +9,3 @@ conda activate AmberTools25
 # Run the test workflow
 echo "Running test workflow..."
 bash test_workflow.sh
-
-## Start JupyterLab
-#cd /workspace/AmberTools
-#jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=$(python3 -c "from jupyter_server.auth import passwd; print(passwd('YOUR_PASSWORD'))")
